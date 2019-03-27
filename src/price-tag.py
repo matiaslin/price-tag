19c88162bdd5a54f5d3c9b84a55f431efb68357b
Matias Lin <matiasenoclin@gmail.com>
2019-03-27 09:39:26 -0700

    Testing file

--- src/price-tag.py
+++ src/price-tag.py
@@ -2,7 +2,7 @@ import urllib
 
 def test():
     connection = urllib.urlopen("https://www.zillow.com/homes/for_sale/globalrelevanceex_sort/"+"33.080754,-117.162838,32.9051,-117.410031_rect/11_zm/")
-    # We have to figure out the coordinates as well as the last "/x/" section
+    # We have to figure out the coordinates as well as the last "/x/" section.
     output = connection.read()
     print(output)
     connection.close()
